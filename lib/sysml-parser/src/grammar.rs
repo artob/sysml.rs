35678f63f968b1ffec162e6b9940d6108dee6f84
Arto Bendiken <arto@bendiken.net>
2024-08-08 15:02:06 +0900

    Hook up the lexer and parser.

--- lib/sysml-parser/src/grammar.rs
+++ lib/sysml-parser/src/grammar.rs
@@ -1,16 +1,17 @@
 // This is free and unencumbered software released into the public domain.
 
+use super::lexer::*;
 use crate::{
     prelude::{String, Vec},
     ParsedBlock, ParsedImport, ParsedMember, ParsedPackage,
 };
 use nom::{
     branch::alt,
-    bytes::complete::{is_not, tag, take_while},
-    character::complete::{alpha1, alphanumeric1, char, multispace0, multispace1},
-    combinator::{map, opt, recognize},
+    bytes::complete::{tag, take_while},
+    character::complete::{char, multispace0, multispace1},
+    combinator::{map, opt},
     multi::many0,
-    sequence::{delimited, pair, preceded, terminated},
+    sequence::{delimited, preceded, terminated},
     IResult,
 };
 use sysml_model::QualifiedName;
@@ -52,8 +53,8 @@ pub fn block_usage(input: &str) -> IResult<&str, ParsedMember> {
     Ok((
         input,
         ParsedMember::BlockUsage(ParsedBlock {
-            name: name.map(String::from),
-            short_name: short_name.map(String::from),
+            name,
+            short_name,
             members,
         }),
     ))
@@ -62,7 +63,7 @@ pub fn block_usage(input: &str) -> IResult<&str, ParsedMember> {
 pub fn element(
     input: &str,
     tag_name: impl AsRef<str>,
-) -> IResult<&str, (Option<&str>, Option<&str>, Vec<ParsedMember>)> {
+) -> IResult<&str, (Option<String>, Option<String>, Vec<ParsedMember>)> {
     let (input, _) = tag(tag_name.as_ref())(input)?;
     let (input, _) = multispace1(input)?;
     let (input, (name, short_name)) = identification(input)?;
@@ -84,31 +85,12 @@ pub fn element(
     Ok((input, (name, short_name, members)))
 }
 
-pub fn qualified_name(input: &str) -> IResult<&str, &str> {
-    name(input) // TODO
-}
-
-pub fn identification(input: &str) -> IResult<&str, (Option<&str>, Option<&str>)> {
+pub fn identification(input: &str) -> IResult<&str, (Option<String>, Option<String>)> {
     let (input, short_name) = opt(delimited(char('<'), name, char('>')))(input)?;
     let (input, _) = multispace0(input)?;
     let (input, name) = opt(name)(input)?;
 
     Ok((input, (name, short_name)))
-}
-
-pub fn name(input: &str) -> IResult<&str, &str> {
-    alt((basic_name, unrestricted_name))(input)
-}
-
-pub fn basic_name(input: &str) -> IResult<&str, &str> {
-    recognize(pair(
-        alt((alpha1, tag("_"))),
-        many0(alt((alphanumeric1, tag("_")))),
-    ))(input)
-}
-
-pub fn unrestricted_name(input: &str) -> IResult<&str, &str> {
-    delimited(char('\''), is_not("'"), char('\''))(input)
 }
 
 #[cfg(test)]
--- lib/sysml-parser/src/lexer.rs
+++ lib/sysml-parser/src/lexer.rs
@@ -1,52 +1,98 @@
 // This is free and unencumbered software released into the public domain.
 
 use super::Keyword;
-use crate::{prelude::String, Error};
+use crate::{
+    prelude::{String, Vec},
+    Error,
+};
 use nom::{
     branch::alt,
     bytes::complete::{is_not, tag, take_while1},
-    character::complete::{alpha1, alphanumeric1, char},
-    combinator::{map_res, recognize},
+    character::complete::{alpha1, alphanumeric1, char, multispace0},
+    combinator::{map, map_res, recognize},
     error::ErrorKind,
-    multi::many0, sequence::{delimited, pair},
-    IResult
+    multi::many0,
+    sequence::{delimited, pair},
+    IResult,
 };
 use sysml_model::QualifiedName;
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq, PartialOrd)]
 pub enum Token {
+    Keyword(Keyword),
     Name(String),
     QualifiedName(QualifiedName),
-    Keyword(Keyword),
 }
 
-pub fn name(input: &str) -> IResult<&str, Token> {
+pub fn tokens(input: &str) -> IResult<&str, Vec<Token>> {
+    many0(delimited(multispace0, token, multispace0))(input)
+}
+
+pub fn token(input: &str) -> IResult<&str, Token> {
+    alt((
+        map(reserved_keyword, Token::Keyword),
+        map(name, Token::Name),
+        map(qualified_name, Token::QualifiedName),
+    ))(input)
+}
+
+pub fn qualified_name(input: &str) -> IResult<&str, QualifiedName> {
+    map(name, QualifiedName::from)(input)
+}
+
+pub fn name(input: &str) -> IResult<&str, String> {
     alt((basic_name, unrestricted_name))(input)
 }
 
-pub fn basic_name(input: &str) -> IResult<&str, Token> {
-    let (input, name) = recognize(
-        pair(
-            alt((alpha1, tag("_"))),
-            many0(alt((alphanumeric1, tag("_"))))
-        )
-    )(input)?;
+pub fn basic_name(input: &str) -> IResult<&str, String> {
+    let (input, name) = recognize(pair(
+        alt((alpha1, tag("_"))),
+        many0(alt((alphanumeric1, tag("_")))),
+    ))(input)?;
 
-    Ok((input, Token::Name(String::from(name))))
+    Ok((input, String::from(name)))
 }
 
-pub fn unrestricted_name(input: &str) -> IResult<&str, Token> {
+pub fn unrestricted_name(input: &str) -> IResult<&str, String> {
     let (input, name) = delimited(char('\''), is_not("'"), char('\''))(input)?;
 
-    Ok((input, Token::Name(String::from(name))))
+    Ok((input, String::from(name)))
 }
 
 pub fn reserved_keyword(input: &str) -> IResult<&str, Keyword> {
-    map_res(
-        take_while1(|c: char| c.is_ascii_lowercase()),
-        |s: &str| Keyword::try_from(s).or_else(|_| Err(Error::new(input, ErrorKind::Tag)))
-    )(input)
+    map_res(take_while1(|c: char| c.is_ascii_lowercase()), |s: &str| {
+        Keyword::try_from(s).or_else(|_| Err(Error::new(input, ErrorKind::Tag)))
+    })(input)
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lex_block_keyword() {
+        let input = r#"block"#;
+        assert_eq!(
+            token(input).map(|(_, token)| token),
+            Ok(Token::Keyword(Keyword::Block))
+        );
+    }
+
+    #[test]
+    fn lex_basic_name() {
+        let input = r#"MyPackage"#;
+        assert_eq!(
+            token(input).map(|(_, token)| token),
+            Ok(Token::Name(String::from("MyPackage")))
+        );
+    }
+
+    #[test]
+    fn lex_quoted_name() {
+        let input = r#"'My Package'"#;
+        assert_eq!(
+            token(input).map(|(_, token)| token),
+            Ok(Token::Name(String::from("My Package")))
+        );
+    }
+}

75dd1d7f96080281e2295a2a451ee101ecc06140
Arto Bendiken <arto@bendiken.net>
2024-08-10 15:52:48 +0900

    Record visibility for parsed imports.

--- lib/sysml-parser/src/parsed_import.rs
+++ lib/sysml-parser/src/parsed_import.rs
@@ -1,19 +1,43 @@
 // This is free and unencumbered software released into the public domain.
 
-use sysml_model::{Element, Import, QualifiedName, Relationship};
+use sysml_model::{Element, Import, QualifiedName, Relationship, VisibilityKind};
 
 #[doc(hidden)]
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct ParsedImport {
-    pub qualified_name: QualifiedName,
+    pub imported_name: QualifiedName,
+    pub import_all: bool,
+    pub visibility: Option<VisibilityKind>,
 }
 
 impl ParsedImport {
     pub fn new(qualified_name: QualifiedName) -> Self {
-        Self { qualified_name }
+        Self {
+            imported_name: qualified_name,
+            import_all: false,
+            visibility: None,
+        }
+    }
+
+    pub fn is_wildcard(&self) -> bool {
+        self.imported_name.last().map_or(false, |name| name == "*")
     }
 }
 
-impl Import for ParsedImport {}
+impl Import for ParsedImport {
+    fn visibility(&self) -> VisibilityKind {
+        self.visibility.unwrap_or(VisibilityKind::Public)
+    }
+
+    fn is_import_all(&self) -> bool {
+        self.import_all
+    }
+
+    fn is_recursive(&self) -> bool {
+        self.imported_name.last().map_or(false, |name| name == "**")
+    }
+}
+
 impl Relationship for ParsedImport {}
+
 impl Element for ParsedImport {}

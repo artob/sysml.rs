813681217058c4def3cfa2e731733f2bd3c28ae9
Arthur Carcano <arthur.carcano@ocamlpro.com>
2025-03-19 17:00:36 +0100

    Check that the test submodule is initialized

--- parser_test_suite/src/main.rs
+++ parser_test_suite/src/main.rs
@@ -8,11 +8,15 @@ fn main() -> anyhow::Result<()> {
     let sysml_submodule_path = Path::new(env!("CARGO_MANIFEST_DIR")).join("SysML-v2-Release");
     let kerml_path = sysml_submodule_path.join("kerml/src/examples/**/*.kerml");
     let sysml_path = sysml_submodule_path.join("sysml/src/examples/**/*.sysml");
-    let input_files =
-        glob(&kerml_path.display().to_string())?.chain(glob(&sysml_path.display().to_string())?);
+    let mut input_files = glob(&kerml_path.display().to_string())?
+        .chain(glob(&sysml_path.display().to_string())?)
+        .peekable();
     let mut oks = 0;
     let mut errs = 0;
     let mut res_table = Table::new();
+    if input_files.peek().is_none() {
+        bail!("No test files found, did you init the git submodule?");
+    }
     res_table.set_header(vec!["File", "Result"]);
     for f in input_files {
         let mut row = Vec::new();
